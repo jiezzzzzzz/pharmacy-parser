7cee2e360389aeeb8eb222df2d37ab49a4499204
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-03-29 23:10:49 +0300

    parsed all medicines starting with the letter a

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 import time
-
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import os
 
-
 options = Options()
 executable_path = os.path.join(os.getcwd(), 'chromedriver', 'chromedriver.exe')
 driver = webdriver.Chrome(options=options, service=Service(executable_path=executable_path))
 
-
-driver.get("https://www.rlsnet.ru/drugs/agapurin-40")
-time.sleep(3)
-pob = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div[1]/div[2]/div[3]/div[23]').text
-print(pob)
-xpath = '/html/body/div[1]/div[6]/div/div[1]/div[2]/div[3]/div[5]/a'
-element = driver.find_element(By.XPATH, xpath)
-driver.execute_script("arguments[0].click();", element)
-
-
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-brutto_name=soup.find(attrs={"class": {"structure-heading"}, 'id': {'formula'}}).text.strip()
-print(brutto_name)
-brutto = driver.find_element(By.XPATH, '/html/body/div[1]/div[5]/div/div[1]/div[2]/div[6]').text
-print(brutto)
+
+driver.get("https://www.rlsnet.ru/drugs/ukazatel/c0")
+time.sleep(3)
+links = []
+
+links_scope = driver.find_elements(By.CSS_SELECTOR, 'a.link')
+for one_link in links_scope:
+    links.append(one_link.get_attribute('href'))
+for link in links[2:]:
+    driver.get(link)
+
+    pob = driver.find_element(By.CSS_SELECTOR, 'div > p.OPIS_POLE').text
+    if len(pob) == 0:
+        print("пусто")
+    else:
+        print(pob)
+    xpath = '/html/body/div[1]/div[6]/div/div[1]/div[2]/div[3]/div[5]/a'
+    element = driver.find_element(By.CSS_SELECTOR, 'a')
+    driver.execute_script("arguments[0].click();", element)
+    brutto = driver.find_element(By.CSS_SELECTOR, 'div:not([class]):nth-of-type(3)').text
+    if len(brutto) == 0:
+        print('пусто')
+    else:
+        print(brutto)
+
